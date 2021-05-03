a20b873aa0d401ac5c4c8a0d59f816b1aebcba1e
giridhar <giridhar7889@gmail.com>
2021-05-03 18:34:33 +0530

    data collected by react and connected to api

--- core/core/settings.py
+++ core/core/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'blog_api',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,3 +128,9 @@ REST_FRAMEWORK ={
         'rest_framework.permissions.AllowAny'
     ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+
+    "http://127.0.0.1:8000",
+    "http://localhost:3000"
+]
