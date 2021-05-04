1efa1281983fc6a22b589ad98683036b3b6dec46
giridhar <giridhar7889@gmail.com>
2021-05-04 22:47:51 +0530

    added custom level permissions

--- core/blog_api/views.py
+++ core/blog_api/views.py
@@ -1,15 +1,29 @@
 from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
+from rest_framework.permissions import BasePermission,SAFE_METHODS,IsAdminUser,DjangoModelPermissionsOrAnonReadOnly
+
+
+class PostUserWritePermission(BasePermission):
+    message='Editing the post is restricted to author only '
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+
+        return obj.author == request.user
+
 
 
 class PostList(generics.ListCreateAPIView):
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
     queryset = Post.postobjects.all()
     serializer_class = PostSerializer
     pass
 
 
-class PostDetail(generics.RetrieveDestroyAPIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView,PostUserWritePermission):
+    permission_classes = [PostUserWritePermission]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     pass
