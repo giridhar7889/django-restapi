6251a17b22b8f24c4bb7812435e9ce6eb56d4c12
giridhar <giridhar7889@gmail.com>
2021-05-13 22:25:14 +0530

    generated schema and documentation with coreapi docs

--- core/core/urls.py
+++ core/core/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -27,6 +29,13 @@ urlpatterns = [
     path('', include('blog.urls',namespace="blog")),
     path('api/', include('blog_api.urls',namespace="blog_api")),
     path('api/user/', include('users.urls',namespace="users")),
-    path('api-auth/', include('rest_framework.urls',namespace="rest_framework"))
+    path('api-auth/', include('rest_framework.urls',namespace="rest_framework")),
+    path('docs/',include_docs_urls(title='BlogAPI')),
+    path('schema', get_schema_view(
+        title="BlogAPI",
+        description="API for the BlogAPI",
+        version="1.0.0"
+    ), name='openapi-schema'),
+
 
 ]
