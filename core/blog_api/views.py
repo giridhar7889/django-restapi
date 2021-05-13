cd8f86fb11cc841935df2b9028dd0f6474d32d36
giridhar <giridhar7889@gmail.com>
2021-05-13 21:55:46 +0530

    added viewsets and routers to api

--- core/blog_api/views.py
+++ core/blog_api/views.py
@@ -1,11 +1,15 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
-from rest_framework.permissions import BasePermission,SAFE_METHODS,IsAdminUser,DjangoModelPermissionsOrAnonReadOnly
+from rest_framework import viewsets
+from rest_framework.permissions import BasePermission, SAFE_METHODS, IsAdminUser, DjangoModelPermissionsOrAnonReadOnly, \
+    IsAuthenticated
+from rest_framework.response import Response
 
 
 class PostUserWritePermission(BasePermission):
-    message='Editing the post is restricted to author only '
+    message = 'Editing the post is restricted to author only '
 
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
@@ -14,16 +18,43 @@ class PostUserWritePermission(BasePermission):
         return obj.author == request.user
 
 
-
-class PostList(generics.ListCreateAPIView):
-    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
-    queryset = Post.postobjects.all()
+class PostList(viewsets.ModelViewSet):
+    permission_classes =[DjangoModelPermissionsOrAnonReadOnly]
     serializer_class = PostSerializer
-    pass
 
+    def get_object(self, queryset=None, **kwargs):
+        item = self.kwargs.get('pk')
+        return get_object_or_404(Post, slug=item)
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView,PostUserWritePermission):
-    permission_classes = [PostUserWritePermission]
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    pass
+    # Define Custom Queryset
+    def get_queryset(self):
+        return Post.objects.all()
+
+#
+# class PostList(viewsets.ViewSet):
+#     permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+#     queryset = Post.postobjects.all()
+#
+#     def list(self, request):
+#         serializer_class = PostSerializer(self.queryset,many=True)
+#         return Response(serializer_class.data)
+#
+#     def retrieve(self, request,pk):
+#         post=get_object_or_404(self.queryset,pk=pk)
+#         serializer_class = PostSerializer(post)
+#         return Response(serializer_class.data)
+#
+#
+
+# class PostList(generics.ListCreateAPIView):
+#     permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+#     queryset = Post.postobjects.all()
+#     serializer_class = PostSerializer
+#     pass
+#
+#
+# class PostDetail(generics.RetrieveUpdateDestroyAPIView,PostUserWritePermission):
+#     permission_classes = [PostUserWritePermission]
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#     pass
--- core/frontend/blogapi/src/index.js
+++ core/frontend/blogapi/src/index.js
@@ -9,6 +9,7 @@ import Footer from './components/Footer';
 import Register from './components/register';
 import Login from './components/login';
 import Logout from './components/logout';
+import Single from './components/single';
 
 const routing = (
 	<Router>
@@ -19,6 +20,7 @@ const routing = (
 				<Route exact path="/register" component={Register} />
 				<Route exact path="/login" component={Login} />
 				<Route exact path="/logout" component={Logout} />
+				<Route path="/post/:slug" component={Single} />
 			</Switch>
 			<Footer />
 		</React.StrictMode>
